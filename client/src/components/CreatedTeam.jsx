d4fadd1abaeac7f0b2ed0488090d70b545dbb412
88bones <123712642+88bones@users.noreply.github.com>
2025-10-12 17:40:04 +0545

    more pretty vpli

--- client/src/components/CreatedTeam.jsx
+++ client/src/components/CreatedTeam.jsx
@@ -21,25 +21,35 @@ const CreatedTeam = () => {
       });
   }, []);
 
-  console.log(teams);
+  //   console.log(teams);
 
   return (
-    <div>
-      <h1>Created Teams</h1>
+    <div className="bg-white w-full shadow-md rounded h-94 p-4 col-span-4 overflow-scroll overflow-x-hidden">
+      <header className="mb-2 font-extrabold text-xl">
+        <p>Created Teams</p>
+      </header>
       {Array.isArray(teams) && teams.length > 0 ? (
-        <div className="flex flex-col bg-white gap-5 w-full">
+        <div className="mt-2 grid grid-cols-3 gap-4 w-full max-sm:w-full  max-sm:grid-cols-1 ">
           {teams.map((item, index) => (
-            <div key={index}>
-              <p>{item.title}</p>
+            <div
+              key={index}
+              className="max-sm:mb-4 w-full rounded shadow-md p-2 ml-2"
+            >
+              <p className="font-extrabold">{item.title}</p>
               {Array.isArray(item.team) ? (
                 item.team.length > 0 ? (
                   <div>
                     {item.team.map((t) => (
-                      <div key={t._id}>{t.firstname}</div>
+                      <div key={t._id} className="flex justify-between">
+                        <p>
+                          {t.firstname} {t.lastname}
+                        </p>
+                        <span>{t.email}</span>
+                      </div>
                     ))}
                   </div>
                 ) : (
-                  "no teams"
+                  "No team made"
                 )
               ) : (
                 ""

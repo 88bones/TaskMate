d5c18768b7eb92ff63a7503a4a5d6ab83015c6b0
88bones <123712642+88bones@users.noreply.github.com>
2025-10-12 17:06:12 +0545

    team fetched frontend have to make pretty

--- client/src/components/CreatedTeam.jsx
+++ client/src/components/CreatedTeam.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTeam } from "../services/getTeam";
 
 const CreatedTeam = () => {
+  const [teams, setTeams] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getTeam()
+      .then((res) => {
+        if (res.message) {
+          setTeams([]);
+          setError("No teams for the project");
+        } else {
+          setTeams(res);
+          setError("");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(teams);
+
   return (
     <div>
       <h1>Created Teams</h1>
+      {Array.isArray(teams) && teams.length > 0 ? (
+        <div className="flex flex-col bg-white gap-5 w-full">
+          {teams.map((item, index) => (
+            <div key={index}>
+              <p>{item.title}</p>
+              {Array.isArray(item.team) ? (
+                item.team.length > 0 ? (
+                  <div>
+                    {item.team.map((t) => (
+                      <div key={t._id}>{t.firstname}</div>
+                    ))}
+                  </div>
+                ) : (
+                  "no teams"
+                )
+              ) : (
+                ""
+              )}
+            </div>
+          ))}
+        </div>
+      ) : (
+        error && <p>{error}</p>
+      )}
     </div>
   );
 };

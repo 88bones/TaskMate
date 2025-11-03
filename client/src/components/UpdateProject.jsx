9ae6c235e50214e7642cc58f622d7824e886b423
88bones <123712642+88bones@users.noreply.github.com>
2025-11-03 14:54:27 +0545

    data displayed in update form

--- client/src/components/UpdateProject.jsx
+++ client/src/components/UpdateProject.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Select from "react-select";
-import { getProject } from "../services/getProject";
+import { getOneProject } from "../services/getProject";
 
 const UpdateProject = () => {
   const inputStyle =
@@ -11,7 +11,7 @@ const UpdateProject = () => {
   const { projectId } = useParams();
   const { _id } = useSelector((state) => state.user);
   const users = useSelector((state) => state.user.users || []);
-  // console.log(users);
+  const [error, setError] = useState("");
 
   const [data, setData] = useState({
     title: "",
@@ -22,9 +22,57 @@ const UpdateProject = () => {
     createdBy: _id,
   });
 
+  // Convert all users into select options
+  const userOptions = users.map((u) => ({
+    value: u._id,
+    label: `${u.firstname} ${u.lastname}`,
+  }));
+
+  //Convert fetched project team into react-select value
+  const selectedTeam = data.team.map((member) => ({
+    value: member._id,
+    label: `${member.firstname} ${member.lastname}`,
+  }));
+
+  // fetch project
   useEffect(() => {
-    getProject(projectId);
-  }, []);
+    getOneProject(projectId)
+      .then((res) => {
+        if (res.message) {
+          setError(res.message);
+        } else {
+          setData({
+            title: res.title,
+            description: res.description,
+            startDate: res.startDate.split("T")[0], // convert to yyyy-mm-dd
+            endDate: res.endDate.split("T")[0],
+            team: Array.isArray(res.team) ? res.team : [],
+            createdBy: _id,
+          });
+          setError("");
+        }
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, [projectId, _id]);
+
+  //input change handler
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  //Team select handler
+  const handleTeamChange = (selected) => {
+    setData((prev) => ({
+      ...prev,
+      team: selected ? selected.map((s) => s.value) : [],
+    }));
+  };
 
   return (
     <div className="p-4 rounded h-fit w-full bg-white shadow-md max-sm:w-full">
@@ -38,18 +86,27 @@ const UpdateProject = () => {
           type="text"
           name="title"
           placeholder="Title"
+          value={data.title}
+          onChange={handleChange}
           required
         />
+
         <textarea
           name="description"
           rows={5}
           placeholder="Description"
+          value={data.description}
+          onChange={handleChange}
           className={inputStyle}
         />
 
         <Select
           className="mb-3 border rounded border-black"
           placeholder="Select team members..."
+          options={userOptions}
+          value={selectedTeam}
+          onChange={handleTeamChange}
+          isMulti
         />
 
         <div className="flex gap-10">
@@ -59,8 +116,8 @@ const UpdateProject = () => {
               type="date"
               name="startDate"
               className="border rounded px-2 py-1 w-40"
-              // value={data.startDate}
-              // onChange={handleChange}
+              value={data.startDate}
+              onChange={handleChange}
             />
           </div>
 
@@ -70,8 +127,8 @@ const UpdateProject = () => {
               type="date"
               name="endDate"
               className="border rounded px-2 py-1 w-40"
-              // value={data.endDate}
-              // onChange={handleChange}
+              value={data.endDate}
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -80,7 +137,7 @@ const UpdateProject = () => {
           className="mt-3 bg-black w-full text-white font-bold py-2 rounded-xl cursor-pointer"
           type="submit"
         >
-          Create Project
+          Update Project
         </button>
       </form>
     </div>
--- client/src/services/getProject.js
+++ client/src/services/getProject.js
@@ -10,3 +10,14 @@ export const getProject = async (userId) => {
     throw err;
   }
 };
+
+export const getOneProject = async (projectId) => {
+  try {
+    const res = await axios.get(
+      `http://localhost:3001/api/project/get-project/${projectId}`
+    );
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
